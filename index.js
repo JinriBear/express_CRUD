2494f6bb43f393076ce5abf54589f4a879170d61
JinriBear <hennurunge@gmail.com>
2025-02-03 14:59:18 +0900

    feat: 프로젝트 초기 셋팅

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.render('index', {data: "Hello World"});
+})
+
+app.listen(PORT, () => {
+  console.log(`Server on... http://localhost:${PORT}`);
+})
+
+
+
