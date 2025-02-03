42172ec838518e2e3f60e3e242437e5b11e65a40
JinriBear <hennurunge@gmail.com>
2025-02-04 03:33:59 +0900

    feat: 게시글 목록 조회 기능 및 기본 ui 구현
    - 게시글 목록을 조회할 수 있는 기능 추가
    - 메인페이지의 ui 구현

--- index.js
+++ index.js
@@ -7,8 +7,21 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true}));
 
+const postData = [
+  {
+    title: "테스트 제목1",
+    content: "테스트 내용1",
+    name: "비회원"
+  },
+  {
+    title: "테스트 제목2",
+    content: "테스트 내용2",
+    name: "비회원"
+  }
+];
+
 app.get('/', (req, res) => {
-  res.render('index', {data: "Hello World"});
+  res.render('index', { postData });
 })
 
 app.listen(PORT, () => {
