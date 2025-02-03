c088fb250fd1228c0e76b9e62825ff28ed7bf09a
JinriBear <hennurunge@gmail.com>
2025-02-03 19:37:57 +0900

    chore: express.urlencoded 미들웨어 추가
    - post 요청의 본문을 파싱하기 위해 추가

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
   res.render('index', {data: "Hello World"});
