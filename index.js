2c7e0b6f6efb3070ede79aeac6a245b25233a5d5
JinriBear <hennurunge@gmail.com>
2025-02-07 01:20:06 +0900

    chore: express.json 미들웨어 추가

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
 const postsData = [
   {
