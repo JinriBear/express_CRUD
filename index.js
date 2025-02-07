e311b0099331a785a305b9fb20e73491ed7cd43e
JinriBear <hennurunge@gmail.com>
2025-02-07 14:25:22 +0900

    featL CORS 허용을 위한 미들웨어 추가

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.use(cors());
 
 const postsData = [
   {
