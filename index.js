8aa568f16f50e83b376b2004658c2fdb39db2d1c
JinriBear <hennurunge@gmail.com>
2025-02-10 15:37:47 +0900

    feat: form태그에 PUT을 사용하기 위해 method-override 미들웨어 추가

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
+var methodOverride = require('method-override')
 const URL = process.env.SERVER_URL
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(cors());
+app.use(methodOverride('_method'))
 
 const postsData = [
   {
